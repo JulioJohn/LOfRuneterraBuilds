6c7902d8ba5906bb92c6a672fb6c710b39b2d8db
Dibas <patia.mello@gmail.com>
2020-02-06 11:20:31 -0300

    Deck builder CollectionView 90% done

---
+++ LOfRuneterraBuilds/Modules/Decks/DeckCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DeckCollectionViewCell.swift
+//  LOfRuneterraBuilds
+//
+//  Created by Patricia Amado Ferreira de Mello on 06/02/20.
+//  Copyright © 2020 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import UIKit
+
+class DeckCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cardSelect: CardSelectController!
+    
+    
+}
