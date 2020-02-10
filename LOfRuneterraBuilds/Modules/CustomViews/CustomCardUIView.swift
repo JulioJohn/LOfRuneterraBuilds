ed2491b005ce441ed3f709cd0310ad83c37b9d4b
Dibas <patia.mello@gmail.com>
2020-02-10 15:42:16 -0300

    CustomCard and Cart Storyboard

---
+++ LOfRuneterraBuilds/Modules/CustomViews/CustomCardUIView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomCardUIView.swift
+//  LOfRuneterraBuilds
+//
+//  Created by Patricia Amado Ferreira de Mello on 10/02/20.
+//  Copyright © 2020 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import UIKit
+
+class CustomCardUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
