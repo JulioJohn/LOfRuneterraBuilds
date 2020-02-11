283aa13018d15f6801202e738c9d419ddc2f12ea
Dibas <patia.mello@gmail.com>
2020-02-11 08:36:52 -0300

    CustomUITableView created

---
+++ LOfRuneterraBuilds/Modules/CustomViews/CustomCardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCardTableViewCell.swift
+//  LOfRuneterraBuilds
+//
+//  Created by Patricia Amado Ferreira de Mello on 10/02/20.
+//  Copyright © 2020 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import UIKit
+
+class CustomCardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
