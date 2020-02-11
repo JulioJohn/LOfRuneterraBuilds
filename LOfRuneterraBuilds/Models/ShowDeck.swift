36dd3ac1f4a8b2471458e60a7a87a0bc5d291a78
Júlio John <julio_john@hotmail.com.br>
2020-02-11 11:01:51 -0300

    DecDetail collection, table done and added Colors and ShowDeck models

---
+++ LOfRuneterraBuilds/Models/ShowDeck.swift
@@ -0,0 +1,21 @@
+//
+//  ShowDeck.swift
+//  LOfRuneterraBuilds
+//
+//  Created by Júlio John Tavares Ramos on 11/02/20.
+//  Copyright © 2020 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import Foundation
+
+class ShowDeck {
+    var champions: [Pack]? = []
+    var spells: [Pack]? = []
+    var followers: [Pack]? = []
+    
+    init(champions: [Pack]?, spells: [Pack]?, followers: [Pack]?) {
+        self.champions = champions
+        self.spells = spells
+        self.followers = followers
+    }
+}
